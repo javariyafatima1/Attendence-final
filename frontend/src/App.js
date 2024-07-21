f70e7516ec70aefad57309fa49a8e902ccde3ab7
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-21 13:06:09 +0500

    bootsrap componenet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter,Route, Routes} from "react-router-dom";
+import Signup from './components/Signup';
+import Login from './components/Login';
 function App() {
   return (
     <BrowserRouter>
