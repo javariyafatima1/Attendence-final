2b604eacba30192f88754dc09ba5a2653b50d7a8
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-21 13:10:12 +0500

    image import

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import image from '../images/saylani image.JPG';
 const Header = () => {
   return (
     <div>
