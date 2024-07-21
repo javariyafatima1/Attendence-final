1076eb1bbc4e31a7aa4c73a94e46811b4e984724
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-21 15:17:28 +0500

    allattendence

--- frontend/src/components/AllAtten.js
+++ frontend/src/components/AllAtten.js
@@ -3,9 +3,40 @@ import { Container, Typography, Table, TableBody, TableCell, TableContainer, Tab
 
 const AllAtten = () => {
     const [attendance, setAttendance] = useState([]);
+    useEffect(() => {
+        setAttendance([
+          { date: '2023-07-01', status: 'Present' },
+          { date: '2023-07-02', status: 'Absent' },
+          { date: '2023-07-01', status: 'Present' },
+          { date: '2023-07-02', status: 'Absent' },
+        ]);
+      }, []);
   return (
     <div>
-      all atten
+        <Container>
+      <Typography variant="h4" gutterBottom> 30 Day Attendance Show</Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {attendance.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell >{item.date}</TableCell>
+                <TableCell className={item.status === 'Absent' ? 'absent' : ''}>
+                  {item.status}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+      
     </div>
   );
 }
