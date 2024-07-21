208d245b79eccb290d8983598a5956c65a90318b
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-21 13:48:19 +0500

    ADshboard work

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,7 +3,11 @@ import { Container, Typography } from '@mui/material';
 const Dashboard = () => {
   return (
     <div>
-    
+      <Container>
+    <Typography variant="h4" gutterBottom>Dashboard</Typography>
+    <Typography>Welcome to the student Attendence portal!</Typography>
+    <AllAtten/>
+  </Container>
     </div>
   );
 }
