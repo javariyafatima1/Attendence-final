216b0f90e4e9ea30ceba7689f552ed7bdf68e437
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-21 14:28:28 +0500

    craete mark satate

--- frontend/src/components/CreateAtten.js
+++ frontend/src/components/CreateAtten.js
@@ -2,6 +2,11 @@
 import React, { useState } from 'react';
 import { TextField, Button, Container, Typography, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 const CreateAtten = () => {
+  const [studentId, setStudentId] = useState('');
+  const [date, setDate] = useState('');
+  const [classname, setclassname] = useState('');
+  const [teacher, setteacher] = useState('');
+  const [status, setStatus] = useState('Present');
   return (
     <div>
       create
