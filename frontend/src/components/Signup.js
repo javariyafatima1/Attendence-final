4de2cad4ab8c91e386ec7a865830cd8a31d485ed
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-21 13:40:23 +0500

    singup state

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { TextField, Button, Container, Typography } from '@mui/material';
 
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setname] = useState('');
   return (
     <div>
       singup
