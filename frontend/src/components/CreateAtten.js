1ad2762a8cac4488a025dc319790925945b72d61
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-21 14:29:28 +0500

    inputs add create component

--- frontend/src/components/CreateAtten.js
+++ frontend/src/components/CreateAtten.js
@@ -8,9 +8,56 @@ const CreateAtten = () => {
   const [teacher, setteacher] = useState('');
   const [status, setStatus] = useState('Present');
   return (
-    <div>
-      create
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant="h4" gutterBottom>Mark Attendance</Typography>
+      <TextField
+        fullWidth
+        margin="normal"
+        label="Student ID"
+        value={studentId}
+        onChange={(e) => setStudentId(e.target.value)}
+      />
+      <TextField
+        fullWidth
+        margin="normal"
+        type="date"
+        label="Date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        InputLabelProps={{ shrink: true }}
+      />
+       <TextField
+        fullWidth
+        margin="normal"
+        label="Class name"
+        value={classname}
+        onChange={(e) => setclassname (e.target.value)}
+      />
+        <TextField
+        fullWidth
+        margin="normal"
+        label="Teacher Name"
+        value={teacher}
+        onChange={(e) => setteacher (e.target.value)}
+      />
+      <FormControl fullWidth margin="normal">
+        
+        <Select
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+        >
+          <MenuItem value="Present">Present</MenuItem>
+          <MenuItem value="Absent">Absent</MenuItem>
+        </Select>
+      </FormControl>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={MarkAttendance}
+      >
+      Attendance Mark
+      </Button>
+    </Container>
   );
 }
 
