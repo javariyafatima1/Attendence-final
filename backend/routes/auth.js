5effb7f8fc99d26d675d1091dc8bbca1b34f2ea8
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-27 07:38:00 +0500

    password convert hashpassword database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 const User = require('../models/user');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 
 router.post('/signup', async (req, res) => {
     const { name, email, password  } = req.body;
     try {
-   
-      const user = new User({ name, email, password });
+        const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ name, email, password:hashedPassword });
       await user.save()
       .then(() => res.status(200).json({ message: 'User created successfully' }))
   } catch (err) {
