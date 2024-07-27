38d27cc647479d3c69ba69614441e12f7c111719
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-27 07:35:57 +0500

    singup api create

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,15 @@
 const User = require('../models/user');
+const router = express.Router();
+
+
+router.post('/signup', async (req, res) => {
+    const { name, email, password  } = req.body;
+    try {
+   
+      const user = new User({ name, email, password });
+      await user.save()
+      .then(() => res.status(200).json({ message: 'User created successfully' }))
+  } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
