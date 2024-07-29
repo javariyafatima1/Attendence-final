a1f9660d9c3665898c3fb6ad70ced84f12a6f490
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-29 21:20:18 +0500

    login api update

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -27,4 +27,45 @@ const signupUser = async (req, res) => {
         })
     }
 }
+const loginUser = async (req, res) => {
+    const { email, password } = req.body
+    console.log("req body", req.body)
+    try {
+       
+        const databaseUser = await userCheck.findOne({ email: email });
+        console.log("Database user found:", databaseUser);
+        if (databaseUser) {
+            const hashedPassword = databaseUser.password;
+            const compareRes = await bycrypt.compare(password, hashedPassword);
+            console.log("Password ", compareRes);
+            if (compareRes) {
+                const JWT_SECRET_KEY = 'attendence';
+                console.log("JWT_SECRET_KEY:", JWT_SECRET_KEY);
+                const token = jwt.sign({ email }, JWT_SECRET_KEY, { expiresIn: '1h' });
+                console.log("Generated token:", token);
+                res.status(200).send({
+                    message: "Login successful",
+                    token: token
+                });
+            }else {
+                console.log("Incorrect password");
+                res.status(401).send({
+                    message: "Incorrect password"
+                });
+            }
+        } else {
+            console.log("Incorrect email");
+            res.status(401).send({
+                message: "Incorrect email"
+            });
+        }
+    } catch (e) {
+        console.log("Login error:", e.message);
+        res.status(500).send({
+            message: e.message,
+        });
+    }
+}
+
+
 
