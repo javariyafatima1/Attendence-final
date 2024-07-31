e371afe65c46ca144d073bb448d9dfff1c879ab8
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-31 20:35:48 +0500

    token autherization

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { Container, Typography } from '@mui/material';
 import AllAtten from './AllAtten';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
 const Dashboard = () => {
   const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-
-  },[])
+    const fetchUserData = async () => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        navigate('/login'); 
+        return;
+    }
+    }
+  },[navigate])
   return (
     <div>
       <Container>
