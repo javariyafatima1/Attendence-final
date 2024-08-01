d14348f98d50053850647c218dd444a0fd819513
javariyafatima1 <hoorfatima2729@gmail.com>
2024-08-01 22:25:23 +0500

    admin portal login

---
+++ frontend/src/components/Adminlogin.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import {    useNavigate,} from "react-router-dom";
+import { TextField, Button, Container, Typography } from '@mui/material';
+import axios from 'axios'
+const Login = () => {
+  const navigate = useNavigate();
+  const [sing, setsing] = useState({
+    email: "",
+    password:"",
+    });
+    
+  
+    const Login = async () => {
+     
+      
+      
+       
+    
+     
+    
+      };
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setsing({ ...sing, [name]: value });
+    }
+
+    return (
+        <div>
+           <Container maxWidth="sm">
+          <Typography variant="h4" gutterBottom>Admin Login</Typography>
+          
+          <TextField
+           fullWidth
+           margin="normal"
+           label="Email"
+           type="email"
+           name='email'
+           value={sing.email}
+           onChange={handleChange}
+          />
+         <TextField
+        fullWidth
+        margin="normal"
+        label="Password"
+        type="password"
+        name='password'
+        value={sing.password}
+        onChange={handleChange}
+      />
+          <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={Login}
+      >
+     Login
+      </Button>
+          </Container>
+          </div>
+    );
+  }
+  export default Login;
