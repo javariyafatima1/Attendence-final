a55f168e2e5cdab89ff0c97ec458fd5205f78d29
javariyafatima1 <hoorfatima2729@gmail.com>
2024-08-01 20:50:17 +0500

    index.js route update

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require ('express')
 const app = express()
 const cors = require('cors');
 const Auth = require('./routes/auth.js')
+const atten = require('./routes/attendencerout.js');
 const PORT = 1000;
 require("./conne/db.js")
 app.use(cors());
 app.use(express.json());
 app.use('/api',Auth);
+app.use('/at',atten);
 app.listen(PORT, () => {
   console.log(`Example app listening on port: `)
 })
