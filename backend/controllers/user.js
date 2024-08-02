ce68a2d3705ef1a51516a01c57ae72a46e3fa23c
javariyafatima1 <hoorfatima2729@gmail.com>
2024-08-02 21:34:27 +0500

    admin controller compelet

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const bycrypt = require('bcrypt');
 const  jwt = require('jsonwebtoken');
 const userCheck = require('../schema/userscema')
-
+const  adminCheck = require('../schema/userscema')
 const signupUser = async (req, res) => {
     const { name, email, password } = req.body
     console.log("req body", req.body)
@@ -28,6 +28,7 @@ const signupUser = async (req, res) => {
         })
     }
 }
+
 const loginUser = async (req, res) => {
     const { email, password } = req.body
     console.log("req body", req.body)
@@ -91,9 +92,75 @@ const getUserData = async (req, res) => {
             res.status(500).send({ message: e.message });
         }
 };
+const Adminsignup = async (req, res) => {
+    const { name, email, password } = req.body
+    console.log("req body", req.body)
+    try {
+        const salt = await bycrypt.genSalt(10)
+        console.log("salt", salt)
+        const hashedPassword = await bycrypt.hash(password, salt)
+        console.log("hashedPassword", hashedPassword)
+        const adminCheckres = await adminCheck({ email, name, password: hashedPassword })
+        const saveRes = await adminCheckres.save()
+        console.log("saveRes", saveRes)
+        res.send({
+            status: 200,
+            message: " admin singup Api is working",
+            saveRes: saveRes,
+            adminCheckres: adminCheckres
+        })
+    }
+    catch (e) {
+        console.log(e.message)
+        res.send({
+            status: 500,
+            message: e.message,
+        })
+    }
+}
+const adminlogin = async (req, res) => {
+    const { email, password } = req.body
+    console.log("req body", req.body)
+    try {
+       
+        const databaseUser = await adminCheck.findOne({ email });
+        console.log("Database user found:", databaseUser);
+        if (databaseUser) {
+            const hashedPassword = databaseUser.password;
+            const compareRes = await bycrypt.compare(password, hashedPassword);
+            console.log("Password ", compareRes);
+            if (compareRes) {
+                const JWT_SECRET_KEY = 'attendence';
+                console.log("JWT_SECRET_KEY:", JWT_SECRET_KEY);
+                const token = jwt.sign({ id: databaseUser._id, email: databaseUser.email }, JWT_SECRET_KEY, { expiresIn: '1h' });
+                console.log("Generated token:", token);
+                res.status(200).send({
+                    message: "Login successful",
+                    token: token
+                });
+            }else {
+                console.log("Incorrect password");
+                res.status(401).send({
+                    message: "Incorrect password"
+                });
+            }
+        } else {
+            console.log("Incorrect email");
+            res.status(401).send({
+                message: "Incorrect email"
+            });
+        }
+    } catch (e) {
+        console.log("Login error:", e.message);
+        res.status(500).send({
+            message: e.message,
+        });
+    }
+}
 module.exports = {
-   
-    signupUser,
+    Adminsignup,
+    adminlogin,
+   signupUser,
     loginUser,
     getUserData
    }; 
