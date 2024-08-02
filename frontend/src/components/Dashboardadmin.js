95f8ced0cd888fd1d3bca168fbafac0aaaf28cff
javariyafatima1 <hoorfatima2729@gmail.com>
2024-08-02 22:03:28 +0500

    admin dashboard name email feach api

--- frontend/src/components/Dashboardadmin.js
+++ frontend/src/components/Dashboardadmin.js
@@ -5,9 +5,42 @@ import axios from 'axios'
 import Logout from './Logout';
 
 const Dashboardadmin = () => {
+  const [adminData, setadminData] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchadminData = async () => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        navigate('/adminlogin'); 
+        return;
+    }
+    try{
+const response = await axios.get('http://localhost:1000/api/getadmindata', {
+      headers: { Authorization: `Bearer ${token}` }
+  });
+  setadminData(response.data.admin);
+}catch(error){
+  console.error('Error admin data:', error);
+  if (error.response?.status === 401) {
+      navigate('/adminlogin');          
+     }
+}
+    }
+    fetchadminData();
+  },[navigate])
+  if (!adminData) return <div>Loading...</div>
   return (
     <div>
-      admin Dashbaord
+    <Container>
+    <Typography variant="h4" gutterBottom> Admin Dashboard</Typography>
+    <Typography>Welcome to the Admin Attendence portal!</Typography>
+    
+            <p>Welcome, {adminData.name}!</p>
+            <p>Email: {admin.email}</p>
+            <Logout/>
+          
+  </Container>
     </div>
   );
 }
