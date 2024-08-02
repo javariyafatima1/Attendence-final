1cd256cd101950cb2494401a7e4c16c4e6ebfc9e
javariyafatima1 <hoorfatima2729@gmail.com>
2024-08-02 21:36:48 +0500

    admin routes api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 const verifyToken = require("../middleware/verifyToken")
 const router = Router();
 
-const { signupUser, loginUser, getUserData } = require("../controllers/user")
+const { signupUser, loginUser, getUserData,Adminsignup,adminlogin, } = require("../controllers/user")
 router.post('/signup', signupUser)
 router.post('/login', loginUser)
 router.get('/getUserData', verifyToken, getUserData)
+router.post('/adminsignup', Adminsignup)
+router.post('/adminlogin', adminlogin)
 
 
 module.exports = router
