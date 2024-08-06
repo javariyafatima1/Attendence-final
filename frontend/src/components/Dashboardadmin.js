4569ce8bea9d7fbf3de56f8079e8e10a9ce45949
javariyafatima1 <hoorfatima2729@gmail.com>
2024-08-06 16:53:58 +0500

    admin dashboard student category waise show

--- frontend/src/components/Dashboardadmin.js
+++ frontend/src/components/Dashboardadmin.js
@@ -3,9 +3,14 @@ import { Container, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import Logout from './Logout';
+import UsersByCategory from './UserbyCategory';
 
 const Dashboardadmin = () => {
-  
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const CategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
 
 
   return (
@@ -15,7 +20,13 @@ const Dashboardadmin = () => {
     <Typography>Welcome to the Admin Attendence portal!</Typography>
     
          
-         
+<div>
+  <h1>User Data</h1>
+<button onClick={() => CategoryClick('GraphicDesign')}>Graphic Design</button>
+      <button onClick={() => CategoryClick('Ai')}>Ai</button>
+      <button onClick={() => CategoryClick('WebDevelopment')}>Web Development</button>
+{selectedCategory && <UsersByCategory category={selectedCategory} />}
+</div>
           
   </Container>
     </div>
