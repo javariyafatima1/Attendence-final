224b647d5604c42cc336ad6f11d34019716df9b1
javariyafatima1 <hoorfatima2729@gmail.com>
2024-08-11 22:42:55 +0500

    vercel update

--- backend/Server.js
+++ backend/Server.js
@@ -4,13 +4,17 @@ const cors = require('cors');
 const Auth = require('./routes/auth.js')
 const atten = require('./routes/attendencerout.js');
 const PORT = 1000;
-
+const path = require("path");
 require("./conne/db.js")
 app.use(cors());
 app.use(express.json());
 app.use('/api',Auth);
 app.use('/at',atten);
 
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port: `)
